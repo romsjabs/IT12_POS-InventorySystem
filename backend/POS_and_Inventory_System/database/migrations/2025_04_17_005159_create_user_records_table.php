a049ce9c7d1ac988e4e2a442c32a7fd523d37330
romsjabs <minecraftjabez@gmail.com>
2025-04-19 16:59:50 +0800

    changes in backend, dashboard users in progress

---
+++ backend/POS_and_Inventory_System/database/migrations/2025_04_17_005159_create_user_records_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_records', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->increments('user_id');
+            $table->string('firstname');
+            $table->string('middlename')->nullable();
+            $table->string('lastname');
+            $table->string('extension')->nullable();
+            $table->enum();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_records');
+    }
+};
